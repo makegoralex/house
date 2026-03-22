618f3b4c191e96f8053b79b0b189639e718d5b8a
root <root@cv5569903.novalocal>
2026-03-22 12:06:04 +0300

    test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div style={{fontFamily: 'Arial', padding: 40}}>
-      <h1>🚀 House работает</h1>
+      <h1>🚀 House работает1</h1>
       <p>Если ты это видишь — деплой настроен правильно</p>
     </div>
   )
